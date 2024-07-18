d0d9695fc965680cded000da9c92c7ca8921c99f
bakhmudov <bakhmudov01@mail.ru>
2024-07-18 10:05:09 +0300

    В миграции create_users_table поле name теперь nullable и значение по умолчанию null

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable()->default(null);
             $table->string('email')->unique();
             $table->boolean('is_active')->default(false);
             $table->timestamp('email_verified_at')->nullable();
