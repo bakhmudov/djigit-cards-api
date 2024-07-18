342ea8f2a27ebb1d587f63e9af647215cb2b30cd
bakhmudov <bakhmudov01@mail.ru>
2024-07-18 10:02:38 +0300

    В RegisterController импортировал класс Mail для отправки письма на почту

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Models\EmailVerification;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
