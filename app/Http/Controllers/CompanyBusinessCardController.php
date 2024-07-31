369a5deae050091f87cd56727feb728825e11b5c
bakhmudov <bakhmudov01@mail.ru>
2024-07-31 16:12:03 +0300

    Изменен метод store в CompanyBusinessCardController: теперь при создании визитки для компании нужно только 'Название'

--- app/Http/Controllers/CompanyBusinessCardController.php
+++ app/Http/Controllers/CompanyBusinessCardController.php
@@ -8,41 +8,37 @@ use Illuminate\Http\Request;
 
 class CompanyBusinessCardController extends Controller
 {
+    /**
+     * Store a new created company business card.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function store(Request $request): JsonResponse
     {
+// Валидация только одного поля "name"
         $data = $request->validate([
-            'logo' => 'nullable|string',
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'addresses' => 'nullable|array',
-            'addresses.*.type' => 'required|string',
-            'addresses.*.address' => 'required|string',
-            'websites' => 'nullable|array',
-            'websites.*.type' => 'required|string',
-            'websites.*.url' => 'required|string',
+            'name' => 'required|string|max:255'
         ]);
 
+// Создание визитки компании с заполнением только поля "name"
         $company = CompanyBusinessCard::create($data);
-
-        if (isset($data['addresses'])) {
-            foreach ($data['addresses'] as $address) {
-                $company->addresses()->create($address);
-            }
-        }
-
-        if (isset($data['websites'])) {
-            foreach ($data['websites'] as $website) {
-                $company->websites()->create($website);
-            }
-        }
 
         return response()->json(['data' => $company], 201);
     }
 
-    public function update(Request $request, $id): JsonResponse
+    /**
+     * Update the specified company business card.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(Request $request, int $id): JsonResponse
     {
         $company = CompanyBusinessCard::findOrFail($id);
 
+// Валидация всех полей, которые могут быть обновлены
         $data = $request->validate([
             'logo' => 'nullable|string',
             'name' => 'required|string|max:255',
@@ -55,8 +51,10 @@ class CompanyBusinessCardController extends Controller
             'websites.*.url' => 'required|string',
         ]);
 
+// Обновление визитки компании
         $company->update($data);
 
+// Обновление адресов
         $company->addresses()->delete();
         if (isset($data['addresses'])) {
             foreach ($data['addresses'] as $address) {
@@ -64,28 +62,42 @@ class CompanyBusinessCardController extends Controller
             }
         }
 
+// Обновление веб-сайтов
         $company->websites()->delete();
         if (isset($data['websites'])) {
             foreach ($data['websites'] as $website) {
                 $company->websites()->create($website);
             }
-        }
+}
 
-        return response()->json(['data' => $company], 200);
+        return response()->json(['data' => $company]);
     }
 
-    public function show($id): JsonResponse
+    /**
+     * Show the specified company business card.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show(int $id): JsonResponse
     {
+// Получение визитки компании с адресами, веб-сайтами и сотрудниками
         $company = CompanyBusinessCard::with(['addresses', 'websites', 'employees'])->findOrFail($id);
 
-        return response()->json(['data' => $company], 200);
+        return response()->json(['data' => $company]);
     }
 
-    public function destroy($id): JsonResponse
+    /**
+     * Remove the specified company business card.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy(int $id): JsonResponse
     {
         $company = CompanyBusinessCard::findOrFail($id);
         $company->delete();
 
-        return response()->json(['data' => 'Company business card deleted successfully'], 200);
+        return response()->json(['data' => 'Company business card deleted successfully']);
     }
 }
