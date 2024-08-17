7b53734ec40c49e5e730d9a2e8f321ae8fd2d6cf
bakhmudov <bakhmudov01@mail.ru>
2024-08-17 23:32:35 +0300

    Изменен метод update в PersonalBusinessCardController, добавлено определение типа как поступили данные через form-data или json

--- app/Http/Controllers/PersonalBusinessCardController.php
+++ app/Http/Controllers/PersonalBusinessCardController.php
@@ -7,6 +7,7 @@ use App\Models\PersonalBusinessCard;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class PersonalBusinessCardController extends Controller
 {
